66dccd8803670a7f26641ba1388b81d214495cf4
taskeenzehra512 <taskeen.zehra@emumba.com>
2024-11-26 15:26:51 +0500

    update runtest path

--- runtest.sh
+++ runtest.sh
@@ -2,9 +2,9 @@
 
 # Run the Newman collection and generate both CLI 
 
-newman run ~/Documents/PROJECT/postman_collection.json \
+newman run ./PROJECT/postman_collection.json \
     --reporters cli,html \
     
 
 # After tests are done, call the Python script to send the email
-python3 /home/emumba/Documents/PROJECT/send_report.py
+python3 ./PROJECT/send_report.py
